c174a1f37f416fd8995ca5298fd3e937c1e4e4a1
Lakshmanan1996 <117020820+Lakshmanan1996@users.noreply.github.com>
2026-06-17 13:17:04 +0530

    Update SuperMarketAppApplication.java

--- src/main/java/com/example/SuperMarketApp/SuperMarketAppApplication.java
+++ src/main/java/com/example/SuperMarketApp/SuperMarketAppApplication.java
@@ -2,13 +2,16 @@ package com.example.SuperMarketApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
+@EntityScan(basePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example")
 public class SuperMarketAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SuperMarketAppApplication.class, args);
-		System.out.println("Super market application");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SuperMarketAppApplication.class, args);
+        System.out.println("Super market application started");
+    }
 }
